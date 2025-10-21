82dad74125359faa6d3c7e15cea1dfbfa211fbe7
U L R Kavindya <it23600898@my.sliit.lk>
2025-10-22 01:53:25 +0530

    Fixed edit product form

--- pages/admin/editProductForm.jsx
+++ pages/admin/editProductForm.jsx
@@ -41,7 +41,7 @@ export default function EditProductPage() {
     const fetchProduct = async () => {
       try {
         const token = localStorage.getItem("token");
-        const { data } = await axios.get(`${import.meta.env.VITE_BACKEND_UR}L/api/products/${productId}`, {
+        const { data } = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/products/${productId}`, {
           headers: { Authorization: `Bearer ${token}` },
         });
 
